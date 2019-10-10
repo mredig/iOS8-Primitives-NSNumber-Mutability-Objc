7ceec3068488b247c018d8b89c9f62cfd54f21bf
Michael Redig <mredig@gmail.com>
2019-10-10 11:37:27 -0500

    added first project

---
+++ iOS8-Pointers/iOS8-Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS8-Pointers
+//
+//  Created by Michael Redig on 10/10/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
