5d27ade882b3984cfd578f0cc83f25d9c0124ba3
Michael Redig <mredig@gmail.com>
2019-10-10 13:23:22 -0500

    c pointer test

--- iOS8-Pointers/iOS8-Pointers/main.m
+++ iOS8-Pointers/iOS8-Pointers/main.m
@@ -8,6 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+// Function declartion
+void cStringExercise(void);
+void pointerTest(void);
+
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+//		pointerTest();
+
+		cStringExercise();
+
+
+	}
+	return 0;
+}
+
+
+// Function definition
+
+// Goal: Print Your Name
+void cStringExercise(void) {
+    char *message = "Hello xxxxxxxxxxxxxxxxxxxxxx!"; // '\0' = null terminated
+
+    printf("message pointer: %p\n", message);
+    printf("message: %s\n", message);
+
+    unsigned long length = strlen(message);
+    char *ptr = malloc(sizeof(char) * length);
+    ptr = strcpy(ptr, message);
+
+    ptr[6] = 'P'; // START HERE
+
+	char *myName = "Michael";
+	unsigned long nameLength = strlen(myName);
+
+	for (int i = 0; i < nameLength; i++) {
+		ptr[6 + i] = myName[i];
+	}
+	ptr[6 + nameLength] = '!';
+	ptr[6 + nameLength + 1] = '\0';
+
+
+     // Set your name!
+     // Null terminate with '\0'
+
+    printf("message: %s\n", ptr);
+
+    free(ptr);
+}
+
 void pointerTest(void) {
 	char letter = 't';
 
@@ -28,14 +78,4 @@ void pointerTest(void) {
 
 	char *ptr = "Hello, world!\n";
 	printf("ptr: %s", ptr);
-}
-
-int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-//		pointerTest();
-
-		
-
-	}
-	return 0;
 }
