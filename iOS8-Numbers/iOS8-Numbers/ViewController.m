8c06eeec54086c69b6984c27e73c5647ea12a229
Michael Redig <mredig@gmail.com>
2019-10-10 13:23:34 -0500

    added ios project for numbers

---
+++ iOS8-Numbers/iOS8-Numbers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS8-Numbers
+//
+//  Created by Michael Redig on 10/10/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
