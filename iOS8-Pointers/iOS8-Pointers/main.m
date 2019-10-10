bac0f216db869f6fd4d65e6301e71d9e5a6f2ab7
Michael Redig <mredig@gmail.com>
2019-10-10 12:41:10 -0500

    pointer examples

--- iOS8-Pointers/iOS8-Pointers/main.m
+++ iOS8-Pointers/iOS8-Pointers/main.m
@@ -8,10 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
+void pointerTest(void) {
+	char letter = 't';
+
+	printf("letter: %c\n", letter);
+
+	long value = 42;
+	printf("value: %li\n", value);
+
+	printf("value: %li\n", value);
+
+	// Pointer to a "long" number
+	long *addressOfValue = &value; // & = address of
+	printf("addressOfValue: %p\n", addressOfValue);
+	printf("&value: %p\n", &value);
+
+	// Dereference the pointer (go to the GPS address)
+	printf("*addressOfValue: %li\n", *addressOfValue);
+
+	char *ptr = "Hello, world!\n";
+	printf("ptr: %s", ptr);
+}
+
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-	    // insert code here...
-	    NSLog(@"Hello, World!");
+//		pointerTest();
+
+		
+
 	}
 	return 0;
 }
